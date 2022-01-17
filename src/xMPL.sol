32941fb1ff256a3459f3dc34f5430d53562330b2
Lucas Manuel <lucas@maple.finance>
2022-01-17 14:59:32 -0500

    feat: set up initial 4626 functions

--- src/xMPL.sol
+++ src/xMPL.sol
@@ -1,4 +1,28 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.7;
 
-contract xMPL {}
+import { ERC20 } from "lib/erc20/src/ERC20.sol";
+
+contract xMPL is ERC20 {
+
+    address constant public MPL = 0x33349B282065b0284d756F0577FB39c158F935e6;
+
+    function depositMPL(uint256 amount) external {
+
+    }
+
+    // transfer
+    // underlying
+    // 4626 style
+
+    // deposit
+    // withdraw
+    // redeem
+    // balanceOfUnderlying
+    // exchangeRate
+    // totalHoldings
+
+    function totalHoldings() external view returns (uint256 totalHoldings_) {
+        return _freeBalance + rateOfIssuance * dTime;  // y = mx + b
+    }
+}

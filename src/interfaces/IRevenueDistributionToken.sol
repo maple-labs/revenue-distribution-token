e9b9b06b44363f77b1c9af589c1ca2f71c132ab9
Erick <erick@maple.finance>
2022-01-26 16:34:28 -0500

    change visibility and add preview functions

--- src/interfaces/IRevenueDistributionToken.sol
+++ src/interfaces/IRevenueDistributionToken.sol
@@ -2,10 +2,17 @@
 pragma solidity ^0.8.7;
 
 interface IRevenueDistributionToken {
-    function deposit(uint256 amount) external;
-    function withdraw(uint256 fundsAssetAmount) external;
-    function redeem(uint256 poolTokenAmount) external;
-    function exchangeRate() external view returns (uint256);
+    event Deposit(address indexed from, uint256 value);
+    event Withdraw(address indexed to, uint256 value);
+
+    function deposit(uint256 amount) external returns (uint256 shares);
+    function withdraw(uint256 fundsAssetAmount) external returns (uint256 shares);
+    function redeem(uint256 poolTokenAmount) external returns (uint256 value);
+    function underlying() external view returns (address);
     function totalHoldings() external view returns (uint256);
     function balanceOfUnderlying(address account) external view returns (uint256);
+    function exchangeRate() external view returns (uint256);
+    function previewDeposit(uint256 underlyingAmount) external view returns (uint256 shareAmount);
+    function previewWithdraw(uint256 underlyingAmount) external view returns (uint256 shareAmount);
+    function previewRedeem(uint256 shareAmount) external view returns (uint256 underlyingAmount);
 }

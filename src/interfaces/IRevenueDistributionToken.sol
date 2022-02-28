4f9b9ba1ac27e6dc368a948a134e2b60a1aa25fe
Erick <erick@maple.finance>
2022-02-28 13:04:37 -0500

    chore: remove functions from IERC4626

--- src/interfaces/IRevenueDistributionToken.sol
+++ src/interfaces/IRevenueDistributionToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.7;
 
-import { IERC20 } from "../../lib/erc20/src/interfaces/IERC20.sol";
+import { IERC4626 } from "./IERC4626.sol";
 
 // TODO: Add natspec + inherit from IERC4626 once spec is well defined.
-interface IRevenueDistributionToken is IERC20 {
+interface IRevenueDistributionToken is IERC4626 {
 
     /**************/
     /*** Events ***/
@@ -17,13 +17,12 @@ interface IRevenueDistributionToken is IERC20 {
     /*** State Variables ***/
     /***********************/
 
-    function freeUnderlying() external view returns (uint256 freeUnderlying_);
+    function freeAssets() external view returns (uint256 freeUnderlying_);
     function issuanceRate() external view returns (uint256 issuanceRate_);
     function lastUpdated() external view returns (uint256 lastUpdated_);
     function owner() external view returns (address owner_);
     function pendingOwner() external view returns (address pendingOwner_);
     function precision() external view returns (uint256 precision_);
-    function underlying() external view returns (address underlying_);
     function vestingPeriodFinish() external view returns (uint256 vestingPeriodFinish_);
 
     /********************************/
@@ -38,10 +37,6 @@ interface IRevenueDistributionToken is IERC20 {
     /*** Staker Functions ***/
     /************************/
 
-    function deposit(uint256 amount_) external returns (uint256 shares_);
-    function redeem(uint256 rdTokenAmount_) external returns (uint256 underlyingAmount_);
-    function withdraw(uint256 underlyingAmount_) external returns (uint256 shares_);
-
     /**********************/
     /*** View Functions ***/
     /**********************/
@@ -49,9 +44,6 @@ interface IRevenueDistributionToken is IERC20 {
     function APR() external view returns (uint256 APR_);
     function balanceOfUnderlying(address account) external view returns (uint256 balanceOfUnderlying_);
     function exchangeRate() external view returns (uint256 exchangeRate_);
-    function previewDeposit(uint256 underlyingAmount) external view returns (uint256 shareAmount);
-    function previewWithdraw(uint256 underlyingAmount) external view returns (uint256 shareAmount);
-    function previewRedeem(uint256 shareAmount) external view returns (uint256 underlyingAmount);
     function totalHoldings() external view returns (uint256 totalHoldings_);
 
 }

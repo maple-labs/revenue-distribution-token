95a4eab50fd901c8e9597d805d1f17ac8d7cfee4
Erick <erick@maple.finance>
2022-02-28 13:00:37 -0500

    chore: alphabetical order in IERC4626

--- src/interfaces/IERC4626.sol
+++ src/interfaces/IERC4626.sol
@@ -59,6 +59,19 @@ interface IERC4626 is IERC20 {
     function mint(uint256 shares_, address receiver_) external returns (uint256 assets_);
 
     /**
+      @notice Burns exactly `shares_` from `owner_` and sends `assets_` of underlying tokens to `receiver_`.
+              MUST emit the Withdraw event.
+              MAY support an additional flow in which the underlying tokens are owned by the Vault contract before the `redeem` execution, and are accounted for during `redeem`.
+              MUST revert if all of `shares_` cannot be redeemed (due to withdrawal limit being reached, slippage, the owner not having enough shares, etc).
+              Note that some implementations will require pre-requesting to the Vault before a withdrawal may be performed. Those methods should be performed separately.
+      @param  shares_   The amount of shares to redeem.
+      @param  receiver_ The address to receive the amount of assets corresponding to the redemption.
+      @param  owner_    The address to burn shares from corresponding to the redemption.
+      @return assets_   The asset amount transferred to `receiver_`.
+    */
+    function redeem(uint256 shares_, address receiver_, address owner_) external returns (uint256 assets_);
+
+    /**
       @notice Burns `shares_` from `owner_` and sends exactly `assets_` of underlying tokens to `receiver_`.
               MUST emit the `Withdraw` event.
               MAY support an additional flow in which the underlying tokens are owned by the Vault contract before the `withdraw` execution, and are accounted for during `withdraw`.
@@ -71,19 +84,6 @@ interface IERC4626 is IERC20 {
       @return shares_   The amount of shares burned from `owner_`.
     */
     function withdraw(uint256 assets_, address receiver_, address owner_) external returns (uint256 shares_);
-
-    /**
-      @notice Burns exactly `shares_` from `owner_` and sends `assets_` of underlying tokens to `receiver_`.
-              MUST emit the Withdraw event.
-              MAY support an additional flow in which the underlying tokens are owned by the Vault contract before the `redeem` execution, and are accounted for during `redeem`.
-              MUST revert if all of `shares_` cannot be redeemed (due to withdrawal limit being reached, slippage, the owner not having enough shares, etc).
-              Note that some implementations will require pre-requesting to the Vault before a withdrawal may be performed. Those methods should be performed separately.
-      @param  shares_   The amount of shares to redeem.
-      @param  receiver_ The address to receive the amount of assets corresponding to the redemption.
-      @param  owner_    The address to burn shares from corresponding to the redemption.
-      @return assets_   The asset amount transferred to `receiver_`.
-    */
-    function redeem(uint256 shares_, address receiver_, address owner_) external returns (uint256 assets_);
 
     /**********************/
     /*** View Functions ***/
@@ -98,13 +98,17 @@ interface IERC4626 is IERC20 {
     function asset() external view returns (address assetTokenAddress_);
 
     /** 
-      @notice Total amount of the underlying asset that is “managed” by Vault.
-              SHOULD include any compounding that occurs from yield.
-              MUST be inclusive of any fees that are charged against assets in the Vault.
+      @notice The amount of assets that the Vault would exchange for the amount of shares provided, in an ideal scenario where all the conditions are met.
+              MUST NOT be inclusive of any fees that are charged against assets in the Vault.
+              MUST NOT show any variations depending on the caller.
+              MUST NOT reflect slippage or other on-chain conditions, when performing the actual exchange.
               MUST NOT revert.
-      @return totalManagedAssets_ The total amount of assets the Vault manages.
+              This calculation MAY NOT reflect the “per-user” price-per-share, and instead should reflect the “average-user’s” price-per-share,
+              meaning what the average user should expect to see when exchanging to and from.
+      @param  shares_ The amount of shares looking to convert.
+      @return assets_ The amount of assets that the Vault would exchange for the amount of shares provided.
     */
-    function totalAssets() external view returns (uint256 totalManagedAssets_);
+    function convertToAssets(uint256 shares_) external view returns (uint256 assets_);
 
     /** 
       @notice The amount of shares that the Vault would exchange for the amount of assets provided, in an ideal scenario where all the conditions are met.
@@ -119,20 +123,6 @@ interface IERC4626 is IERC20 {
     */
     function convertToShares(uint256 assets_) external view returns (uint256 shares_);
 
-
-    /** 
-      @notice The amount of assets that the Vault would exchange for the amount of shares provided, in an ideal scenario where all the conditions are met.
-              MUST NOT be inclusive of any fees that are charged against assets in the Vault.
-              MUST NOT show any variations depending on the caller.
-              MUST NOT reflect slippage or other on-chain conditions, when performing the actual exchange.
-              MUST NOT revert.
-              This calculation MAY NOT reflect the “per-user” price-per-share, and instead should reflect the “average-user’s” price-per-share,
-              meaning what the average user should expect to see when exchanging to and from.
-      @param  shares_ The amount of shares looking to convert.
-      @return assets_ The amount of assets that the Vault would exchange for the amount of shares provided.
-    */
-    function convertToAssets(uint256 shares_) external view returns (uint256 assets_);
-
     /**
       @notice Maximum amount of the underlying asset that can be deposited into the Vault for the `receiver_`, through a `deposit` call.
               MUST return a limited value if `receiver_` is subject to some deposit limit.
@@ -142,6 +132,35 @@ interface IERC4626 is IERC20 {
       @return maxAssets_ The max input amount of assets for deposit for a user.
     */
     function maxDeposit(address receiver_) external view returns (uint256 maxAssets_);
+
+    /**
+      @notice Maximum amount of the Vault shares that can be minted for the `receiver_`, through a `mint` call.
+              MUST return a limited value if `receiver` is subject to some mint limit.
+              MUST return `2 ** 256 - 1` if there is no limit on the maximum amount of shares that may be minted.
+              MUST NOT revert.
+      @param  receiver_  The mint recipient.
+      @return maxShares_ The max shares that can be minted for the `receiver_`.
+    */
+    function maxMint(address receiver_) external view returns (uint256 maxShares_);
+
+    /**
+      @notice Maximum amount of Vault shares that can be redeemed from the `owner_` balance in the Vault, through a `redeem` call.
+              MUST return a limited value if `owner_` is subject to some withdrawal limit or timelock.
+              MUST return `balanceOf(owner_)` if `owner_` is not subject to any withdrawal limit or timelock.
+              MUST NOT revert.
+      @param  owner_     The owner of the vault shares.
+      @return maxShares_ The max shares out in a redeem for a user
+    */
+    function maxRedeem(address owner_) external view returns (uint256 maxShares_);
+
+    /**
+      @notice Maximum amount of the underlying asset that can be withdrawn from the `owner` balance in the Vault, through a `withdraw` call.
+              MUST return a limited value if `owner` is subject to some withdrawal limit or timelock.
+              MUST NOT revert.
+      @param  owner_     The owner of assets to withdraw.
+      @return maxAssets_ The max amount of assets `owner_` can withdraw.
+    */
+    function maxWithdraw(address owner_) external view returns (uint256 maxAssets_);
 
     /**
       @notice Allows an on-chain or off-chain user to simulate the effects of their deposit at the current block, given current on-chain conditions.
@@ -155,7 +174,7 @@ interface IERC4626 is IERC20 {
               slippage in share price or some other type of condition,meaning the depositor will lose assets by depositing.
       @param  assets_ The input amount of assets.
       @return shares_ the corresponding amount of shares out from a deposit call with `assets_` in
-     */
+    */
     function previewDeposit(uint256 assets_) external view returns (uint256 shares_);
 
     /**
@@ -170,24 +189,8 @@ interface IERC4626 is IERC20 {
               in share price or some other type of condition, meaning the depositor will lose assets by minting.
       @param  shares_ The amount of shares to be minted.
       @return assets_ The amount of assets corresponding to the mint call.
-     */
+    */
     function previewMint(uint256 shares_) external view returns (uint256 assets_);
-
-    /**
-      @notice Allows an on-chain or off-chain user to simulate the effects of their withdrawal at the current block, given current on-chain conditions.
-              MUST return as close to and no fewer than the exact amount of Vault shares that would be burned in a `withdraw` call in the same transaction.
-              I.e. `withdraw` should return the same or fewer `shares_` as `previewWithdraw` if called in the same transaction.
-              MUST NOT account for withdrawal limits like those returned from maxWithdraw and should always act as though the withdrawal would be accepted,
-              regardless if the user has enough shares, etc.
-              MUST be inclusive of withdrawal fees. Integrators should be aware of the existence of withdrawal fees.
-              MUST NOT revert.
-              Note that any unfavorable discrepancy between `convertToShares` and `previewWithdraw` SHOULD be considered slippage 
-              in share price or some other type of condition, meaning the depositor will lose assets by depositing.
-      @param  assets_ The input amount of `asset` to withdraw.
-      @return shares_ The corresponding amount of shares out from a withdraw call with `assets_` in.
-     */
-    function previewWithdraw(uint256 assets_) external view returns (uint256 shares_);
-
 
     /**
       @notice Allows an on-chain or off-chain user to simulate the effects of their redeemption at the current block, given current on-chain conditions.
@@ -205,33 +208,27 @@ interface IERC4626 is IERC20 {
     function previewRedeem(uint256 shares_) external view returns (uint256 assets_);
 
     /**
-      @notice Maximum amount of the Vault shares that can be minted for the `receiver_`, through a `mint` call.
-              MUST return a limited value if `receiver` is subject to some mint limit.
-              MUST return `2 ** 256 - 1` if there is no limit on the maximum amount of shares that may be minted.
+      @notice Allows an on-chain or off-chain user to simulate the effects of their withdrawal at the current block, given current on-chain conditions.
+              MUST return as close to and no fewer than the exact amount of Vault shares that would be burned in a `withdraw` call in the same transaction.
+              I.e. `withdraw` should return the same or fewer `shares_` as `previewWithdraw` if called in the same transaction.
+              MUST NOT account for withdrawal limits like those returned from maxWithdraw and should always act as though the withdrawal would be accepted,
+              regardless if the user has enough shares, etc.
+              MUST be inclusive of withdrawal fees. Integrators should be aware of the existence of withdrawal fees.
               MUST NOT revert.
-      @param  receiver_  The mint recipient.
-      @return maxShares_ The max shares that can be minted for the `receiver_`.
+              Note that any unfavorable discrepancy between `convertToShares` and `previewWithdraw` SHOULD be considered slippage 
+              in share price or some other type of condition, meaning the depositor will lose assets by depositing.
+      @param  assets_ The input amount of `asset` to withdraw.
+      @return shares_ The corresponding amount of shares out from a withdraw call with `assets_` in.
     */
-    function maxMint(address receiver_) external view returns (uint256 maxShares_);
+    function previewWithdraw(uint256 assets_) external view returns (uint256 shares_);
 
-
-    /**
-      @notice Maximum amount of the underlying asset that can be withdrawn from the `owner` balance in the Vault, through a `withdraw` call.
-              MUST return a limited value if `owner` is subject to some withdrawal limit or timelock.
+    /** 
+      @notice Total amount of the underlying asset that is “managed” by Vault.
+              SHOULD include any compounding that occurs from yield.
+              MUST be inclusive of any fees that are charged against assets in the Vault.
               MUST NOT revert.
-      @param  owner_     The owner of assets to withdraw.
-      @return maxAssets_ The max amount of assets `owner_` can withdraw.
+      @return totalManagedAssets_ The total amount of assets the Vault manages.
     */
-    function maxWithdraw(address owner_) external view returns (uint256 maxAssets_);
-
-    /**
-      @notice Maximum amount of Vault shares that can be redeemed from the `owner_` balance in the Vault, through a `redeem` call.
-              MUST return a limited value if `owner_` is subject to some withdrawal limit or timelock.
-              MUST return `balanceOf(owner_)` if `owner_` is not subject to any withdrawal limit or timelock.
-              MUST NOT revert.
-      @param  owner_     The owner of the vault shares.
-      @return maxShares_ The max shares out in a redeem for a user
-    */
-    function maxRedeem(address owner_) external view returns (uint256 maxShares_);
+    function totalAssets() external view returns (uint256 totalManagedAssets_);
 
 }

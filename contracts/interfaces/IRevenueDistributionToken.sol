97d1f8c130c2be03bbd46bbfb2eca91cb9486eee
Vedran Bidin <vedran@bidin.me>
2022-03-03 02:13:14 -0500

    docs: add RDT documentation

--- contracts/interfaces/IRevenueDistributionToken.sol
+++ contracts/interfaces/IRevenueDistributionToken.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.7;
 import { IERC4626 } from "./IERC4626.sol";
 import { IERC20Permit } from "../../modules/erc20/contracts/interfaces/IERC20Permit.sol";
 
-/// @title ...
+/// @title A token that represents ownership of future revenues distributed linearly over time.
 interface IRevenueDistributionToken is IERC4626, IERC20Permit {
 
     /***********************/
@@ -12,37 +12,38 @@ interface IRevenueDistributionToken is IERC4626, IERC20Permit {
     /***********************/
 
     /**
-     *  @dev ...
+     *  @dev The total amount of the underlying asset that is currently unlocked.
      */
     function freeAssets() external view returns (uint256 freeAssets_);
 
     /**
-     *  @dev ...
+     *  @dev The estimated rate of the vesting schedule that is currently active.
+     *       Denominated as the amount of underlying assets vesting per second.
      */
     function issuanceRate() external view returns (uint256 issuanceRate_);
 
     /**
-     *  @dev ...
+     *  @dev The last time the vesting schedule has been updated.
      */
     function lastUpdated() external view returns (uint256 lastUpdated_);
 
     /**
-     *  @dev ...
+     *  @dev The address of the account that is allowed to update the vesting schedule.
      */
     function owner() external view returns (address owner_);
 
     /**
-     *  @dev ...
+     *  @dev The next owner, nominated by the current owner.
      */
     function pendingOwner() external view returns (address pendingOwner_);
 
     /**
-     *  @dev ...
+     *  @dev The precision at which the issuance rate is measured.
      */
     function precision() external view returns (uint256 precision_);
 
     /**
-     *  @dev ...
+     *  @dev The end of the current vesting schedule.
      */
     function vestingPeriodFinish() external view returns (uint256 vestingPeriodFinish_);
 
@@ -51,21 +52,22 @@ interface IRevenueDistributionToken is IERC4626, IERC20Permit {
     /********************************/
 
     /**
-     *  @dev ...
+     *  @dev Sets the pending owner as the new owner.
+     *       Can be called only by the pending owner, and only after his nomination by the current owner.
      */
     function acceptOwnership() external;
 
     /**
-     *  @dev   ...
-     *  @param account_ ...
+     *  @dev   Sets a new address as the pending owner.
+     *  @param pendingOwner_ The next potential owner.
      */
-    function setPendingOwner(address account_) external;
+    function setPendingOwner(address pendingOwner_) external;
 
     /**
-     *  @dev    ...
-     *  @param  vestingPeriod_ ...
-     *  @return issuanceRate_  ...
-     *  @return freeAssets_    ...
+     *  @dev    Updates the current vesting schedule based on how much time has elapsed since the last update.
+     *  @param  vestingPeriod_ The amount of time over which all so far unaccounted underlying assets will be vested over.
+     *  @return issuanceRate_  The new issuance rate.
+     *  @return freeAssets_    The new amount of underlying assets that are unlocked.
      */
     function updateVestingSchedule(uint256 vestingPeriod_) external returns (uint256 issuanceRate_, uint256 freeAssets_);
 
@@ -74,15 +76,14 @@ interface IRevenueDistributionToken is IERC4626, IERC20Permit {
     /**********************/
 
     /**
-     *  @dev    ...
-     *  @return APR_ ...
+     *  @dev Returns the annualized yearly return of the current vesting schedule.
      */
     function APR() external view returns (uint256 APR_);
 
     /**
-     *  @dev    ...
-     *  @param  account_ ...
-     *  @return assets_  ...
+     *  @dev    Returns the amount of underlying assets owned by the specified account.
+     *  @param  account_ Address of the account.
+     *  @return assets_  Amount of assets owned.
      */
     function balanceOfAssets(address account_) external view returns (uint256 assets_);
 

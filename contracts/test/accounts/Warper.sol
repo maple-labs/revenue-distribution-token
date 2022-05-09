08b2f1cb62ed08fd9513e500f505b3fbd51300a0
Vedran Bidin <vedran@bidin.me>
2022-05-09 18:31:46 +0300

    test: add additional invariant tests

--- contracts/test/accounts/Warper.sol
+++ contracts/test/accounts/Warper.sol
@@ -3,10 +3,22 @@ pragma solidity ^0.8.7;
 
 import { TestUtils } from "../../../modules/contract-test-utils/contracts/test.sol";
 
+import { IRevenueDistributionToken } from "../../interfaces/IRevenueDistributionToken.sol";
+
 contract Warper is TestUtils {
+
+    address internal _rdt;
+
+    constructor(address rdt_) {
+        _rdt = rdt_;
+    }
 
     function warp(uint256 warpTime_) external {
         vm.warp(block.timestamp + constrictToRange(warpTime_, 1, 100 days));
+    }
+
+    function warpAfterVesting(uint256 warpTime_) external {
+        vm.warp(block.timestamp + IRevenueDistributionToken(_rdt).vestingPeriodFinish() + constrictToRange(warpTime_, 1, 100 days));
     }
 
 }
